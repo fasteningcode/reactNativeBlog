990dc9ca15c3a7e6eb5b8f431828f0f60db9656b
Aadhi <aadhi@fasteningcode.com>
2020-03-24 14:43:55 +1300

    blog posts add with useState

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { Text, View, StyleSheet, FlatList, Button } from 'react-native'
+import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext)
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title='Add Blog Post' onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   )
 }
